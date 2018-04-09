c42206c71ff865ca7c0f54b7e98ade7973780e21
vsominen <35541505+vsominen@users.noreply.github.com>
2018-04-09 16:56:46 -0400

    pom.xml file for github Issues

--- src/main/java/GitHubRestClient.java
+++ src/main/java/GitHubRestClient.java
@@ -20,7 +20,6 @@ import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.junit.Test;
 
 public class GitHubRestClient {
 
